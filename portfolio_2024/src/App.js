d0269a401dd669c70dd08ecbd9b6d2490b479b5d
Lara Forlani <larikaaa@Laras-Air.home>
2024-08-30 00:06:35 +0200

    project reset

--- portfolio_2024/src/App.js
+++ portfolio_2024/src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Homepage } from "./pages/Homepage/Homepage";
-import { About } from "./pages/About/About";
-import { Navbar } from "./components/Navbar/Navbar";
-import { Footer } from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Homepage />} />
-        <Route path="/contacts" element={<Homepage />} />
-      </Routes>
       <Footer />
     </>
   );
