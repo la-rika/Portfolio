ea8d15dfaa652b9e3ef6e905e5e790451128bac5
Lara Forlani <larikaaa@Mac.home>
2025-01-15 09:57:10 +0100

    wip fixes on timeline

--- portfolio_2024/src/Skills.jsx
+++ portfolio_2024/src/Skills.jsx
@@ -4,17 +4,58 @@ import Skill from "./components/SkillComponent/SkillComponent";
 import { ReactComponent as Html } from "./images/html.svg"
 import { ReactComponent as Css } from "./images/css.svg"
 import { ReactComponent as Javascript } from "./images/javascript.svg"
+import { ReactComponent as Bootstrap } from "./images/bootstrap.svg"
+import { ReactComponent as React } from "./images/react.svg"
+import { ReactComponent as Node } from "./images/node.svg"
+
+const skills = [
+    {
+        name: "html",
+        src: <Html />,
+        progress: 95
+    },
+    {
+        name: "css",
+        src: <Css />,
+        progress: 85
+    },
+    {
+        name: "javascript",
+        src: <Javascript />,
+        progress: 70
+    },
+    {
+        name: "bootstrap",
+        src: <Bootstrap />,
+        progress: 70
+    },
+    {
+        name: "react",
+        src: <React />,
+        progress: 90
+    },
+    {
+        name: "node",
+        src: <Node />,
+        progress: 70
+    }
+]
 
 const Skills = () => {
     return (
         <Element id='skills' name='skills'>
             <Container>
-                <h1 className="text-white font-fancy-text text-xl mb-5">SKILLS</h1>
-                <div className="grid grid-cols-3 gap-x-16 ">
-                    <Skill skillImg={<Html />} progress={"w-[95%]"} name={"HTML"} level={"Advanced"}/>
-                    <Skill skillImg={<Css />} progress={"w-[80%]"} name={"CSS"} level={"Advanced"}/>
-                    <Skill skillImg={<Javascript />} progress={"w-[70%]"} name={"Javascript"} level={"Intermediate"}/>
+                <h1 className="text-white font-fancy-text text-xl ">SKILLS</h1>
+                <p className="text-white mb-12">Nothing is at 100% because there's always something new to learn !</p>
+
+                <div className="grid grid-cols-3 gap-x-16 gap-y-10 mb-10"> 
+                    {/* prende il progress solo del primo o di nessuno in base a come gli gira  */}
+                    {skills.map(el => (
+                        <Skill skillImg={el.src} progress={el.progress.toString()} name={el.name.toUpperCase()}
+                            level={el.progress <= 40 ? "Beginner" : el.progress > 40 && el.progress <= 70 ? "Intermediate" : "Advanced"} />
+                    ))}
                 </div>
+
             </Container>
         </Element>
     )
--- portfolio_2024/src/components/ProgressBar/ProgressBar.jsx
+++ portfolio_2024/src/components/ProgressBar/ProgressBar.jsx
@@ -1,8 +1,9 @@
 
 const ProgressBar = ({progress})=>{
+    console.log(progress)
     return(
         <div className="h-3 w-full rounded-full bg-shadow-pink">
-            <div className={`bg-light-pink rounded-full h-full ${progress}`}></div>
+            <div className={`bg-light-pink rounded-full h-full w-[${progress}%]`}></div>
         </div>
     )
 }
