abe6ff7f6b1dff66c6ebaa6b46dd6dbe57b5c87c
Lara Forlani <larikaaa@Mac.home>
2025-01-16 23:16:20 +0100

    finished skills page

--- portfolio_2024/src/Skills.jsx
+++ portfolio_2024/src/Skills.jsx
@@ -13,7 +13,10 @@ import { ReactComponent as Ejs } from "./images/ejs.svg"
 import { ReactComponent as Scss } from "./images/scss.svg"
 import { ReactComponent as Tailwind } from "./images/tailwind.svg"
 import { ReactComponent as Typescript } from "./images/typescript.svg"
-
+import { ReactComponent as Storybook } from "./images/storybook.svg"
+import { ReactComponent as Sql } from "./images/sql.svg"
+import { ReactComponent as Postgre } from "./images/postgre.svg"
+import { ReactComponent as Mongodb } from "./images/mongodb.svg"
 
 
 const skills = [
@@ -46,8 +49,60 @@ const skills = [
         name: "node",
         src: <Node />,
         progress: '70'
-    }
+    },
+    {
+        name: "npm",
+        src: <Npm />,
+        progress: '40'
+    },
+    {
+        name: "express",
+        src: <Express />,
+        progress: '30'
+    },
+    {
+        name: "ejs",
+        src: <Ejs />,
+        progress: '30'
+    },
+    {
+        name: "scss",
+        src: <Scss />,
+        progress: '80'
+    },
+    {
+        name: "tailwind",
+        src: <Tailwind />,
+        progress: '80'
+    },
+    {
+        name: "typescript",
+        src: <Typescript />,
+        progress: '40'
+    },
+    {
+        name: "storybook",
+        src: <Storybook />,
+        progress: '40'
+    },
+    {
+        name: "mongodb",
+        src: <Mongodb />,
+        progress: '20'
+    },
+    {
+        name: "postgre",
+        src: <Postgre />,
+        progress: '20'
+    },
+    {
+        name: "sql",
+        src: <Sql />,
+        progress: '20'
+    },
 ]
+
+//se le barre dei db sono ancora buggate mettere 40%
 
 const Skills = () => {
     return (
@@ -56,10 +111,48 @@ const Skills = () => {
                 <h1 className="text-white font-fancy-text text-xl ">SKILLS</h1>
                 <p className="text-white mb-12">Nothing is at 100% because there's always something new that is waiting to be learned !</p>
 
+                {/* <div className="grid grid-cols-3 gap-x-16 gap-y-10 mb-10">
+                    <Skill skillImg={<Html />} progress={`w-[90%]`} name={"HTML"}
+                        level="Advanced" />
+
+                    <Skill skillImg={<Css />} progress={`w-[90%]`} name={"CSS"}
+                        level="Advanced" />
+
+                    <Skill skillImg={<Javascript />} progress={`w-[70%]`} name={"JAVASCRIPT"}
+                        level="Intermediate" />
+
+                    <Skill skillImg={<Bootstrap />} progress={`w-[70%]`} name={"BOOTSTRAP"}
+                        level="Intermediate" />
+
+                    <Skill skillImg={<React />} progress={`w-[85%]`} name={"REACT"}
+                        level="Advanced" />
+
+                    <Skill skillImg={<Node />} progress={`w-[40%]`} name={"NODE"}
+                        level="Beginner" />
+
+                    <Skill skillImg={<Npm />} progress={`w-[40%]`} name={"NPM"}
+                        level="Beginner" />
+
+                    <Skill skillImg={<Express />} progress={`w-[30%]`} name={"EXPRESS"}
+                        level="Beginner" />
+
+                    <Skill skillImg={<Ejs />} progress={`w-[30%]`} name={"EJS"}
+                        level="Beginner" />
+
+                    <Skill skillImg={<Scss />} progress={`w-[80%]`} name={"SCSS"}
+                        level="Advanced" />
+
+                    <Skill skillImg={<Tailwind />} progress={`w-[80%]`} name={"TAILWIND"}
+                        level="Beginner" />
+
+                    <Skill skillImg={<Typescript />} progress={`w-[40%]`} name={"TYPESCRIPT"}
+                        level="Beginner" />
+                </div> */}
+
                 <div className="grid grid-cols-3 gap-x-16 gap-y-10 mb-10">
                     {skills.map(el => (
                         <Skill skillImg={el.src} progress={`w-[${el.progress}%]`} name={el.name.toUpperCase()}
-                            level={Number(el.progress) <= 40 ? "Beginner" : Number(el.progress) > 40 && Number(el.progress) <= 70 ? "Intermediate" : "Advanced"} />
+                            level={Number(el.progress) <= 40 ? "Beginner" : Number(el.progress) > 40 && Number(el.progress) <= 80 ? "Intermediate" : "Advanced"} />
                     ))}
                 </div>
 
--- portfolio_2024/src/components/SkillComponent/SkillComponent.jsx
+++ portfolio_2024/src/components/SkillComponent/SkillComponent.jsx
@@ -1,15 +1,16 @@
-import ProgressBar from "../ProgressBar/ProgressBar";
 
 const SkillComponent = ({ skillImg, progress, name, level }) => {
 
     return (
-        <div className="flex items-center"> 
+        <div className="flex items-center">
             <div>
                 {skillImg}
             </div>
             <div className="w-full ml-2">
                 <p className="mb-2 text-white">{name}  -  {level}</p>
-                <ProgressBar progress={`${progress}`} />
+                <div className="h-3 w-full rounded-full bg-shadow-pink">
+                    <div className={`bg-light-pink rounded-full h-full ${progress}`}></div>
+                </div>
             </div>
         </div>
     )
