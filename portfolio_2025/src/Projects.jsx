5b217dbbdba8f375842f12b63d069adb7d7dc364
Lara Forlani <larikaaa@Mac.home>
2025-05-02 00:57:51 +0200

    finished custom carousel

--- portfolio_2025/src/Projects.jsx
+++ portfolio_2025/src/Projects.jsx
@@ -1,37 +1,46 @@
 import { Element } from "react-scroll";
 import Container from "./components/Container";
 import ProjectCard from "./components/ProjectCard/ProjectCard";
+import { ReactComponent as Mix1 } from "./images/mix1.svg";
+import { ReactComponent as Mix2 } from "./images/mix2.svg";
+import { ReactComponent as Mix3 } from "./images/mix3.svg";
+import { ReactComponent as Quiz1 } from "./images/quiz1.svg";
+import { ReactComponent as Quiz2 } from "./images/quiz2.svg";
+import { ReactComponent as Quiz3 } from "./images/quiz3.svg";
+import { ReactComponent as Recipes1 } from "./images/recipes1.svg";
+import { ReactComponent as Recipes2 } from "./images/recipes2.svg";
+import { ReactComponent as Recipes3 } from "./images/recipes3.svg";
+
 
 const projects = [
     {
-        title: "Project title",
+        title: "Mix It Up!",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida augue at accumsan vestibulum.",
         link: "github link",
         source: "github link",
-        screenshots:[
-            "portfolio_2025/src/images/mixItUpApp/mix1.png",
-            "portfolio_2025/src/images/mixItUpApp/mix2.png",
-            "portfolio_2025/src/images/mixItUpApp/mix3.png",
+        screenshots: [
+            <Mix1 className="w-full h-full" />, <Mix2 className="w-full h-full" />, <Mix3 className="w-full h-full" />
         ]
     },
     {
-        title: "Project title",
+        title: "Recipes App",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida augue at accumsan vestibulum.",
         link: "github link",
         source: "github link",
-        screenshots:[
-
+        screenshots: [
+            <Recipes1 className="w-full h-full" />, <Recipes2 className="w-full h-full" />, <Recipes3 className="w-full h-full" />
         ]
     },
     {
-        title: "Project title",
+        title: "Bts Quiz",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida augue at accumsan vestibulum.",
         link: "github link",
         source: "github link",
-        screenshots:[
-
+        screenshots: [
+            <Quiz1 className="w-full h-full" />, <Quiz2 className="w-full h-full" />, <Quiz3 className="w-full h-full" />
         ]
     },
+
 ]
 
 
@@ -41,8 +50,8 @@ const Projects = () => {
             <Container>
                 <h1 className="text-white font-fancy-text text-xl mb-8">PROJECTS</h1>
                 <div className="flex flex-col gap-y-14">
-                    {projects.map((el)=>(
-                        <ProjectCard title={el.title} content={el.content} link={el.link} sourceCode={el.source} imgs={el.screenshots}/>
+                    {projects.map((el) => (
+                        <ProjectCard title={el.title} content={el.content} link={el.link} sourceCode={el.source} imgs={el.screenshots} />
                     ))}
                 </div>
             </Container>
--- portfolio_2025/src/components/ProjectCard/ProjectCard.jsx
+++ portfolio_2025/src/components/ProjectCard/ProjectCard.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import Chip from "../Chip/Chip";
-
+import { ReactComponent as Arrow } from "../../images/arrow.svg"
 
 const ProjectCard = ({ title, content, link, sourceCode, imgs }) => {
+    const [count, setCount] = useState(0)
 
     return (
-        <div className="w-full grid grid-cols-3 gap-16 text-white">
+        <div className="w-full flex text-white">
+            <div className="flex items-center gap-4 hover:cursor-pointer ">
+                <button onClick={() => count !== 0 && setCount(count - 1)}>
+                    <Arrow className={`w-8 h-8 rotate-180 hover:cursor-pointer ${count === 0 && "grayscale hover:cursor-not-allowed"}`} />
+                </button>
+                <div>
+                    {imgs.map((el, index) => {
+                        return (
+                            count === index && <div>{el}</div>
+                        )
+                    })}
+                    <div className="flex gap-2 justify-center">
+                        <div className={`h-2 mt-4 w-8 rounded-full ${count === 0 ? "bg-light-pink" : "border-2 border-light-pink"}`}></div>
+                        <div className={`h-2 mt-4 w-8 rounded-full ${count === 1 ? "bg-light-pink" : "border-2 border-light-pink"}`}></div>
+                        <div className={`h-2 mt-4 w-8 rounded-full ${count === 2 ? "bg-light-pink" : "border-2 border-light-pink"}`}></div>
+                    </div>
+                </div>
 
+                <button onClick={() => count < imgs.length - 1 && setCount(count + 1)}>
+                    <Arrow className={`w-8 h-8 hover:cursor-pointer ${count === imgs.length - 1 && "grayscale hover:cursor-not-allowed"}`} />
+                </button>
+            </div>
 
             <div className="flex flex-col gap-y-4 justify-center col-span-2 ml-8">
                 <p className="text-lg font-medium ">{title} </p>
